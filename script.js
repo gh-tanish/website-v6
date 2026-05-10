f6f487972ea62b147669e9c130998528c3912df4
gh-tanish <gm.tanish.paul@gmail.com>
2026-05-10 20:38:48 +0100

    added a javascript set light mode gradients and styles in css

---
+++ script.js
@@ -0,0 +1,35 @@
+const toggleBtn = document.getElementById("theme-toggle");
+
+/* LOAD SAVED MODE */
+if (localStorage.getItem("theme") === "light") {
+  document.body.classList.add("light-mode");
+
+  if (toggleBtn) {
+    toggleBtn.textContent = "⏾ Dark Mode";
+  }
+}
+
+/* BUTTON CLICK */
+if (toggleBtn) {
+
+  toggleBtn.addEventListener("click", () => {
+
+    document.body.classList.toggle("light-mode");
+
+    /* SAVE MODE */
+    if (document.body.classList.contains("light-mode")) {
+
+      localStorage.setItem("theme", "light");
+
+      toggleBtn.textContent = "⏾ Dark Mode";
+
+    } else {
+
+      localStorage.setItem("theme", "dark");
+
+      toggleBtn.textContent = "☀ Light Mode";
+    }
+
+  });
+
+}
